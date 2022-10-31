dabb90de9d3d9ce60951ec7f9d3530bd358eba32
robin mahmud <robinmahmud358@gmail.com>
2022-10-31 10:41:37 +0600

    dynamic card create

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -4,15 +4,15 @@ import parts from '../../../assets/images/about_us/parts.jpg'
 const About = () => {
     return (
         <div>
-            <div className="hero min-h-screen bg-base-200">
+            <div className="hero my-20">
                 <div className="hero-content flex-col lg:flex-row">
                     <div className='w-1/2 relative'>
                         <img src={person} alt='' className="w-4/5 h-full rounded-lg shadow-2xl" />
                         <img src={parts} alt='' className="absolute w-3/5 right-5 top-1/2 border-8 rounded-lg shadow-2xl" />
                     </div>
                     <div className='w-1/2'>
-                        <h1 className='text-2xl font-bold text-warning'>About Us</h1>
-                        <h1 className="text-5xl font-bold">
+                        <h1 className='text-2xl font-bold text-orange-600'>About Us</h1>
+                        <h1 className=" my-5 text-5xl font-bold">
                             We are qualified <br />
                             & of experience <br />
                             in this field
