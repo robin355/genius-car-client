6809c12c574bd3f893196e123d970bce82922801
robin mahmud <robinmahmud358@gmail.com>
2022-10-31 11:39:02 +0600

    firebase and Login  pages design

--- src/Pages/Home/Services/ServiceCard/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard/ServiceCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ServiceCard = ({ service }) => {
     const { img, price, title } = service;
     return (
-        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+        <div className="card card-compact w-full bg-base-100 shadow-xl">
             <figure><img src={img} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -15,7 +15,7 @@ const Services = () => {
                 <h2 className="text-5xl font-semibold">Our Service Area</h2>
                 <p>the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which don't look even slightly believable. </p>
             </div>
-            <div className='grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
                 {
                     services.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
                 }
